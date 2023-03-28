997347fd35f06f5082d97c5aecd837dfece405e5
Logan Fasano <58754739+LFasano0129@users.noreply.github.com>
2023-03-28 19:57:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -27,35 +27,24 @@ scissors = '''
 ---.__(___)
 '''
 # list to store choices
-sign = [rock, paper, scissors]
+game_signs = [rock, paper, scissors]
 # input for player choice
-choice = input("What do you choose? Type 0 for rock, 1 for paper, 2 for scissors: ")
-choice_int = int(choice)
+user_choice = int(input("What do you choose? Type 0 for rock, 1 for paper, 2 for scissors: "))
 # random function for computer choice
 comp_choice = random.randint(0, 2)
-comp_choice_int = int(comp_choice)
-# print statements for displaying choices
-print(f"Your chose:\n {sign[choice_int]}\n")
-print(f"The computer chose:\n {sign[comp_choice_int]}")
 # if and elif statements to display results
-if choice_int == 0:
-    if comp_choice_int == 0:
-        print("Draw")
-    elif comp_choice_int == 1:
-        print("You Lose!")
-    elif comp_choice_int == 2:
-        print("You Win!")
-elif choice_int == 1:
-    if comp_choice_int == 0:
-        print("You Win!")
-    elif comp_choice_int == 1:
-        print("Draw")
-    elif comp_choice_int == 2:
-        print("You Lose!")
-elif choice_int == 2:
-    if comp_choice_int == 0:
-        print("You Lose!")
-    elif comp_choice_int == 1:
-        print("You Win!")
-    elif comp_choice_int == 2:
-        print("Draw")
+if user_choice >= 3 or user_choice < 0:
+    print("You typed an invalid number, you lose!")
+else:
+    print(game_signs[user_choice])
+    print(f"Computer chose:\n {game_signs[comp_choice]}")
+    if user_choice == 0 and comp_choice == 2:
+        print("You win!")
+    elif comp_choice == 0 and user_choice == 2:
+        print("You lose!")
+    elif comp_choice > user_choice:
+        print("You lose!")
+    elif user_choice > comp_choice:
+        print("You win!")
+    elif user_choice == comp_choice:
+        print("It's a draw")
